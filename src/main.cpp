90f2c810d149cc2e6d184e92a59f7d4ec928a065
sebatt90 <sebattack90@gmail.com>
2022-08-18 13:47:08 +0200

    Rendered rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
+#include <GL/glew.h>
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 
 #include "log/log.h"
 
@@ -9,6 +10,7 @@ int main(void)
 
     /* Initialize the library */
     if (!glfwInit()){
+        Logging::PrintErr("GLFW Init failed");
         return -1;
     }
 
@@ -16,19 +18,84 @@ int main(void)
     window = glfwCreateWindow(640, 480, "Sebatt Model Viewer", NULL, NULL);
     if (!window)
     {
+        Logging::PrintErr("Window or OpenGL context creation failed");
         glfwTerminate();
         return -1;
     }
 
-
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    Logging::Print("Window Initialized");
+
+
+    /* TRIANGLE RENDERING */
+    float points[] = {
+        -0.5f,   0.5f,   0.0f,
+        0.5f,   -0.5f,  0.0f,
+        -0.5f,  -0.5f,  0.0f
+    };
+    
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    
+    const char* vertex_shader =
+    "#version 400\n"
+    "in vec3 vp;"
+    "void main() {"
+    "  gl_Position = vec4(vp, 1.0);"
+    "}";
+
+    const char* fragment_shader =
+    "#version 400\n"
+    "out vec4 frag_colour;"
+    "void main() {"
+    "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+    "}";
+
+    /* Compiling the GLSL shaders */
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    /* Creating an empty program to attach the shaders */
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
+
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Rendering the triangle */
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
+        // draw points 0-3 from the currently bound VAO with current in-use shader
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // update other events like input handling 
+        glfwPollEvents();
+        // put the stuff we've been drawing onto the display
+
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
